ce15cbe60f0212ed2f14ebf215b54497b1a3fdeb
Gabriela johnson <johnsongabriela12@gmail.com>
2020-08-14 00:47:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,7 +66,18 @@ class Student
       SELECT *
       FROM students 
       WHERE grade = 9 
-    SQL 
-    
-  end 
+    SQL
+    DB[:conn].execute(sql).map do |row| self.new_from_db(row)
+  end
+  
+   def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 12
+    SQL
+
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
 end
