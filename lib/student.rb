2e9716e032319ffbe4f7395ccf4f2db9bce81d24
Gabriela johnson <johnsongabriela12@gmail.com>
2020-08-14 00:55:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -81,14 +81,28 @@ class Student
       self.new_from_db(row)
     end
   
-   def self.first_X_students_in_grade_10()
+   def self.first_X_students_in_grade_10(num)
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade < 12
+      WHERE grade = 10 
+      LIMIT ?
     SQL
 
     DB[:conn].execute(sql).collect do |row|
       self.new_from_db(row)
     end
+    
+     def self.first_X_students_in_grade_10(num)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10 
+      LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
+    
 end
