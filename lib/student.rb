40b949971481df26c178e6e17255bfd12d6901c5
Gabriela johnson <johnsongabriela12@gmail.com>
2020-08-14 00:56:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -105,4 +105,16 @@ class Student
       self.new_from_db(row)
     end
     
+    def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10 
+      LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
+    
 end
